d6370e250a7e361bc4e8f7a5315861ea7b045378
Matheus Alves <mtalves27@gmail.com>
2022-12-22 21:25:04 -0300

    feat: application skeleton created

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
+    define: {
+      "process.plataform": JSON.stringify(process.platform),
+    },
     css: {
       postcss: {
         plugins: [
