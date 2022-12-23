20317a0ad9ba2bd98de406945cd84e07dd894b2d
Matheus Alves <mtalves27@gmail.com>
2022-12-22 22:39:46 -0300

    feat: document page setup

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { ToC } from "../components/ToC";
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
-      <Link to="/">Acessar blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0 ">
+        <span className="text-rotion-300 font-semiboldbold uppercase text-xs">
+          WORKSPACE
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Arquitetura back-end</ToC.Link>
+          <ToC.Section>Banco de Dados</ToC.Section>
+          <ToC.Section>Mensageria</ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex-col items-center"></section>
     </main>
   );
 }
